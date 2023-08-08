e2a19596d6d206feea6f83850b1f672f12e90f8d
Lam Nguyen <lamndelille@gmail.com>
2023-08-07 23:59:58 -0400

    final final update of the night with full page

--- src/app/placeholder.tsx
+++ src/app/placeholder.tsx
@@ -63,35 +63,48 @@ export default function Page() {
                     <MyHeart/>Support me! <MyHeart/>
                 </Button>
                 { voteValue >= 10 && 
+                        <Fade in timeout={300}>
                     <Typography variant='h6' sx={{ 'marginTop': '1rem', 'color': 'green'}}>
                         Wow, Thanks so much for clicking me over 10 times :)
                     </Typography>
+                        </Fade>
                 }
                 { voteValue >= 25 && 
+                        <Fade in timeout={300}>
                 <Typography variant='h2' sx={{ 'marginTop': '1rem', 'color': 'red', 'textShadow': '2px 2px #555555'}}>
                     25 times... :O
                 </Typography>
+                        </Fade>
                 }
                 { voteValue >= 50 && <>
-
+                        <Fade in timeout={200}><div>
                 <Typography variant='h3' align='center' sx={{ 'marginTop': '1rem', 'color': 'black'}}>
                     50!! 
                 </Typography>
+                        </div></Fade>
+
+                        <Fade in timeout={700}><div>
                 <Typography variant='h5' align='center' sx={{ 'color': 'black'}}>
                     <Divider sx={{'borderColor': 'white'}}/>
                     You win, here&apos;s a song I liked recently for a reward.
                     <Divider sx={{'borderColor': 'white'}}/>
                     I hope you enjoyed clicking the button
                 </Typography>
+
+                        </div></Fade>
+                        <Fade in timeout={1500}><Stack alignItems='center'>
                 <Divider sx={{'borderColor': 'white', 'height': '3rem'}}/>
-               <iframe width="560" height="315" src="https://www.youtube.com/embed/5LbFdY6vGsQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> 
-                        </>}
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/5LbFdY6vGsQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> 
                 <Typography variant='h4' align='center' sx={{ 'marginTop': '1rem', 'color': 'black'}}>
                     Check back in a little bit okay 🫵
                 </Typography>
                 <Typography variant='h6' align='center' sx={{ 'marginTop': '1rem', 'width': '40rem', 'color': 'black'}}>
                     Going to have some cool data science, math, visualizations and more! All at a low price of free ninety nine
                 </Typography>
+                        </Stack>
+                        </Fade>
+                        </>
+                }
             </Stack>
             </Card>
         </Box>
